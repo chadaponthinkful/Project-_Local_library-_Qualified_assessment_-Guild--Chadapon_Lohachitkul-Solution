6233b6162e579d6600195d2691eff3d235073000
chdaponthinkful <dagota2928@gmail.com>
2023-10-13 17:52:26 -0700

    Upload from local without node_modules
    Need to upload from computer since try to use git failed authentication

---
+++ public/src/accounts.js
@@ -0,0 +1,84 @@
+let accounts = [
+  {
+  "id": "5f446f2ecfaf0310387c9603",
+  "name": {
+    "first": "Esther",
+    "last": "Tucker"
+  },
+  "picture": "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
+  "age": 25,
+  "company": "ZILLACON",
+  "email": "esther.tucker@zillacon.me",
+  "registered": "Thursday, May 28, 2015 2:51 PM"
+}, 
+    {
+  "id": "1111",
+  "name": {
+    "first": "Test",
+    "last": "Test"
+  },
+  "picture": "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
+  "age": 25,
+  "company": "ZILLACON",
+  "email": "esther.tucker@zillacon.me",
+  "registered": "Thursday, May 28, 2015 2:51 PM"
+},
+]
+
+
+
+function findAccountById(accounts, id) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+  return accounts.find((account) => account.id === id)
+
+}
+
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+
+    return accounts.sort((accountA, accountB) =>   accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1);
+}
+
+
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  return accounts.map((fullName) => `${fullName.name.first} ${fullName.name.last}`); 
+
+}
+
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
